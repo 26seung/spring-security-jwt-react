08e62324cc8be734962716dac3b9c78fb1ca86d1
Euseung <yousi7@naver.com>
2023-03-31 23:32:27 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Admin from './pages/user/Admin';
+import JoinForm from './pages/user/JoinForm';
+import LoginForm from './pages/user/LoginForm';
+import Profile from './pages/user/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="" element={''} />
+        <Route exact path="/login" element={<LoginForm />} />
+        <Route exact path="/join" element={<JoinForm />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/admin" element={<Admin />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
