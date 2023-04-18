b9ac5166778d2ca9ce177eeac7bd34cf68f3e751
Euseung <yousi7@naver.com>
2023-04-18 23:31:26 +0900

    craco

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+};
