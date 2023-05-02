f5524f8f88cfc1decd4f1ded24476c7934a4e139
Euseung <yousi7@naver.com>
2023-05-02 23:54:44 +0900

    service

--- src/service/auth/AuthHeader.js
+++
@@ -1,9 +0,0 @@
-export const authHeader = () => {
-  const header = localStorage.getItem('Authorization');
-  console.log('AuthHeader', header);
-  //   if (header) {
-  //     return header;
-  //   }
-};
-
-export default authHeader;
