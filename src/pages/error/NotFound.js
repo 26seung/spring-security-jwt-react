20a4130be89dabb3c177152ca2b8e28a5413906b
Euseung <yousi7@naver.com>
2023-05-03 21:45:03 +0900

    error

--- src/pages/error/NotFound.js
+++ src/pages/error/NotFound.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import { Box, Button, Container, Typography } from '@mui/material';
 
 const NotFound = () => {
-  return <div>404 Page</div>;
+  return (
+    <div>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%',
+        }}
+      >
+        <Container maxWidth="md">
+          <Box
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <Box
+              sx={{
+                mb: 3,
+                textAlign: 'center',
+              }}
+            >
+              <img
+                alt="Under development"
+                src="/errors/error-404.png"
+                style={{
+                  display: 'inline-block',
+                  maxWidth: '100%',
+                  width: 400,
+                }}
+              />
+            </Box>
+            <Typography align="center" sx={{ mb: 3 }} variant="h3">
+              404: The page you are looking for isn’t here
+            </Typography>
+            <Typography align="center" color="text.secondary" variant="body1">
+              You either tried some shady route or you came here by mistake.
+              Whichever it is, try using the navigation
+            </Typography>
+            <Button href="/" sx={{ mt: 5 }} variant="contained">
+              ← Go back to page
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </div>
+  );
 };
 
 export default NotFound;
